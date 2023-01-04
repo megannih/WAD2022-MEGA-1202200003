fbfdf432ed22076cbd15bfd7712132c1149210cc
megannih <84509538+megannih@users.noreply.github.com>
2023-01-04 22:46:50 +0700

    REMEDIAL

---
+++ MODUL2 MEGA REMEDIAL/contact.php
@@ -0,0 +1,10 @@
+<div class="page">
+ <h2>Hubungi Kami</h2>
+ <p>Telp: 0274-221133
+  <br>WA: 0813334455
+  <br>Senin - Minggu
+ 24 Jam Non Stop</p>
+
+ <p>Jl. Telekomunikasi no.1 Bojongsoang, kab.Bandung
+ 55581</p>
+</div>
